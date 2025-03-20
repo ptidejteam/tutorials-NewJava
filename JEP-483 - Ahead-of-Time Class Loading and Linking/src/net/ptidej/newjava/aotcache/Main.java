8919b9013032d26a07b9f903c6df3c928c30837b
yann-gael <yann-gael.gueheneuc@concordia.ca>
2025-03-19 22:36:08 -0400

    Added examples for JEP-472 and JEP-483

---
+++ JEP-483 - Ahead-of-Time Class Loading and Linking/src/net/ptidej/newjava/aotcache/Main.java
@@ -0,0 +1,10 @@
+package net.ptidej.newjava.aotcache;
+
+public class Main {
+
+	public static void main(final String[] args) {
+		System.out.println(
+				"Some application doing lots of things at start up...");
+	}
+
+}
